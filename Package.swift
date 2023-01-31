73dfd99bf6de610c469206ada8c6b6120d809d6b
Brian Rojas <marchinram@gmail.com>
2023-01-31 11:14:45 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Movement",
-            url: "http://",
+            url: "https://",
             checksum: ""
         )
     ]
